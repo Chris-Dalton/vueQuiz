cc8a859ac11cfb0e09f3b97c86bbf2dca4c38b10
Chris Dalton <chris.dalton1207@gmail.com>
2020-07-29 12:06:12 -0400

    Initial Commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template v-slot:lead>
+                {{currentQuestion.question}}
+            </template>
+
+            <hr class="my-4">
+
+            <p v-for="(answer, index) in answers" :key="index">
+                
+                {{ answer }}
+            </p>
+
+            <b-button variant="primary" href="#">Submit Button</b-button>
+            <b-button @click="next" href="#">
+                Next Question
+            </b-button>
+
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    }
+}
+</script>
